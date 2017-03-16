477277cd789d49846818958f0e9d2c0a1d2ac972
Michael Bruce <mike@bearmetalcoding.com>
2017-03-16 15:03:21 +0000

    Clean up

--- rplugin/ruby/ice.rb
+++ rplugin/ruby/ice.rb
@@ -1,19 +1,12 @@
 require_relative '../../lib/nrepl_client'
 
 Neovim.plugin do |plug|
-  # Define a command called "SetLine" which sets the contents of the current
-  # line. This command is executed asynchronously, return value ignored.
-  plug.command(:SetLine, :nargs => 1) do |nvim, str|
-    nvim.current.line = str
-  end
-
   plug.command(:TestSend, :nargs => 0) do |nvim, str|
     nvim.current.line = "response : #{test_clone}"
   end
 
   plug.command(:TT, :nargs => '?') do |nvim, str|
     nvim.current.line = "response: #{test_send}"
-    # nvim.message("response: #{decoded}")
   end
 
   plug.command(:Eval, :nargs => '?') do |nvim, str|
@@ -22,9 +15,5 @@ Neovim.plugin do |plug|
 
   plug.command(:Methods, :nargs => 0) do |nvim, str|
     nvim.current.line = "methods: #{nvim.methods}"
-  end
-
-  plug.command(:WAT, :nargs => 0) do |nvim, str|
-    nvim.command('echo "WAT"')
   end
 end
