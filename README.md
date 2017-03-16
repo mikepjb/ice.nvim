19fda045bcac3b90099e9f9ee64ede6f3157d1cb
Michael Bruce <mike@bearmetalcoding.com>
2017-03-16 01:36:59 +0000

    Extract bencode to lib

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-# vim-ice
+# ice.nvim
 
-Integrated Clojure Evaluator
+Integrated Clojure Evaluator for Neovim.
 
 ### Issues
     - :UpdateRemotePlugins will fail if you don't have ruby with access to the 'neovim' gem
--- rplugin/ruby/ice.rb
+++ rplugin/ruby/ice.rb
@@ -1,27 +1,8 @@
 require 'socket'
+require_relative '../../lib/bencode'
 
 socket = TCPSocket.open('127.0.0.1', 9999)
 
-def decode_message(response) # XXX Mike: unit test this
-  next_length = nil
-  response_array = []
-
-  response[1..-2].split(':').each do |message_part|
-    if next_length.nil?
-      next_length = message_part.to_i
-    else
-      payload = message_part[0..(next_length - 1)]
-      response_array << payload
-      next_meta = message_part[next_length..-1]
-      if next_meta =~ /^[0-9].*/
-        next_length = next_meta.to_i
-      else
-        next_length = next_meta[1..-1].to_i
-      end
-    end
-  end
-  Hash[*response_array]
-end
 
 Neovim.plugin do |plug|
   # Define a command called "SetLine" which sets the contents of the current
@@ -43,9 +24,11 @@ Neovim.plugin do |plug|
     decoded = decode_message(response)
     session = decoded["new-session"]
     session_length = decoded["new-session"].length
-    socket.sendmsg "d4:code15:(def devil 666)2:id7:test-id2:op4:eval7:session#{session_length}:#{session}e"
+    socket.sendmsg "d4:code15:(def devil 777)2:id7:test-id2:op4:eval7:session#{session_length}:#{session}e"
     response = socket.recvmsg.first
     decoded = decode_message(response)
     nvim.current.line = "response: #{decoded}"
+    # nvim.message("response: #{decoded}")
+    # nvim.current.line = (nvim.methods - Object.methods).to_s
   end
 end
