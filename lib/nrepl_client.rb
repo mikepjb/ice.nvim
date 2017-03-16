14cf2f4016faa1087edf210c0f69d978495b43a9
Michael Bruce <mike@bearmetalcoding.com>
2017-03-16 15:00:46 +0000

    Successfully calling echo in nvim

--- lib/nrepl_client.rb
+++ lib/nrepl_client.rb
@@ -12,11 +12,11 @@ def test_send
   socket = TCPSocket.open('127.0.0.1', 9999)
   socket.sendmsg 'd2:op5:clonee'
   response = socket.recvmsg.first
-  decoded = Bencode::decode_message(response)
+  decoded = Bencode::decode(response)
   session = decoded["new-session"]
   session_length = decoded["new-session"].length
   socket.sendmsg "d4:code15:(def devil 777)2:id7:test-id2:op4:eval7:session#{session_length}:#{session}e"
   response = socket.recvmsg.first
-  decoded = Bencode::decode_message(response)
+  decoded = Bencode::decode(response)
   decoded
 end
--- rplugin/ruby/ice.rb
+++ rplugin/ruby/ice.rb
@@ -11,8 +11,20 @@ Neovim.plugin do |plug|
     nvim.current.line = "response : #{test_clone}"
   end
 
-  plug.command(:TT, :nargs => 0) do |nvim, str|
+  plug.command(:TT, :nargs => '?') do |nvim, str|
     nvim.current.line = "response: #{test_send}"
     # nvim.message("response: #{decoded}")
+  end
+
+  plug.command(:Eval, :nargs => '?') do |nvim, str|
+    nvim.command("echo \"#{test_send["value"].gsub('"', '\"')}\"")
+  end
+
+  plug.command(:Methods, :nargs => 0) do |nvim, str|
+    nvim.current.line = "methods: #{nvim.methods}"
+  end
+
+  plug.command(:WAT, :nargs => 0) do |nvim, str|
+    nvim.command('echo "WAT"')
   end
 end
