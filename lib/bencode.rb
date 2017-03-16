b693840e6f046a6ccb3d21f35af83ba63ae0072e
Michael Bruce <mike@bearmetalcoding.com>
2017-03-16 15:47:47 +0000

    Clean up

--- lib/bencode.rb
+++ lib/bencode.rb
@@ -27,15 +27,4 @@ module Bencode
     end
     encoded_message + 'e'
   end
-
-  message =
-    {"id" => "test-id",
-     "ns" => "boot.user",
-     "session" => "a647fb12-54ae-4313-8358-1161810de8f",
-     "value" => "#'boot.user/devil"}
-
-      "d2:id7:test-id"\
-      "2:ns9:boot.user"\
-      "7:session36:a647fb12-54ae-4313-8358-1161810de8f"\
-      "35:value17:#'boot.user/devile"
 end
