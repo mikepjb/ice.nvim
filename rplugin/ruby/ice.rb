ab4765be2e045125e960778246c1de74ad758f37
Michael Bruce <mike@bearmetalcoding.com>
2017-03-17 08:29:49 +0000

    Eval works under visual & argument use

--- rplugin/ruby/ice.rb
+++ rplugin/ruby/ice.rb
@@ -1,20 +1,36 @@
 require_relative '../../lib/nrepl_client'
 
+def parse_command_arguments(nvim, arg1, arg2)
+  code_to_evaluate = []
+  if arg1.to_i == 0
+    code_to_evaluate << arg1
+  else
+    (arg1.to_i..arg2.to_i).each do |line_number|
+      code_to_evaluate << nvim.get_current_buffer.get_line(line_number - 1)
+    end
+  end
+  code_to_evaluate.join("\n")
+end
+
 Neovim.plugin do |plug|
-  plug.command(:TestSend, :nargs => 0) do |nvim, args|
-    nvim.current.line = "response : #{test_clone}"
-  end
-
-  plug.command(:TT, :nargs => '?') do |nvim, args|
-    nvim.current.line = "response: #{test_send}"
-  end
-
-  # plug.command(:Eval, :nargs => '?', :range => true) do |nvim, args, range_from, range_to, other|
   # XXX E116: Invalid arguments for function remote#define#CommandBootstrap where quotes are used "
   # XXX note that this error does not occur after the first Eval without " occurs, then including " is fine?!?
-  plug.command(:Eval, :nargs => '?') do |nvim, args|
-    nvim.current.line = "args: #{args}"
-    # nvim.current.line = "args: #{args}, range: #{range_from}-#{range_to}, other: #{other}"
+  # plug.command(:Eval, :nargs => '?') do |nvim, args|
+  # XXX can you collect all args like &args or similar?
+  plug.command(:Eval, :nargs => '?', :range => true) do |nvim, arg1, arg2|
+    # parse_command_arguments => collect_payload/code
+    # nvim.current.line = "args: #{args}"
+    # All from line 1
+    # '<,'>Eval on single line == arg1: 1, arg2: 1
+    # Eval (def hello 345) == arg1: (def hello 345), arg2: 1
+    # .,.+1Eval == arg1: 1, arg2: 2
+
+    code = parse_command_arguments(nvim, arg1, arg2)
+    nvim.command("echo \"#{code}\"")
+    # nvim.current.line = "code: #{options}"
+
+    # nvim.current.line = "arg1: #{arg1}, arg2: #{arg2}"
+
     # if args.nil?
     #   nvim.command("echo \"#{test_send}\"")
     # else
@@ -24,10 +40,20 @@ Neovim.plugin do |plug|
   end
 
   plug.command(:Methods, :nargs => 0) do |nvim, args|
-    nvim.current.line = "methods: #{nvim.methods}"
+    # nvim.current.line = "methods: #{nvim.methods}"
+    nvim.current.line = "methods: #{nvim.get_current_buffer.methods}"
+  end
+
+  plug.command(:GetLine, :nargs => 0) do |nvim, args|
+    # nvim.current.line = "methods: #{nvim.methods}"
+    nvim.current.line = "line: #{nvim.get_current_buffer.get_line(1)}"
   end
 
   plug.command(:Str, :nargs => '?') do |nvim, args|
     nvim.command "echo \"#{args}\""
+  end
+
+  plug.command(:TT, :nargs => '?') do |nvim, args|
+    nvim.current.line = "response: #{test_send}"
   end
 end
