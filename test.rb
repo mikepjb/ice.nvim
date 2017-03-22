e1ee74ea1c4c1de02be1726e0253c8e599bbfcb7
Michael Bruce <mike@bearmetalcoding.com>
2017-03-22 10:08:34 +0000

    Updated test script

--- test.rb
+++ test.rb
@@ -8,27 +8,53 @@ clojure_namespaced_method =
   "(ns watermarker.core)"\
   "\n"\
   "(defn this-method [x]"\
-  "   (+ 37 77 x))"\
+  "   (+ 37 77 x))"
 
-socket = send(clojure_namespaced_method)
 
-@messages = []
+failing_method =
+  "(ns watermarker.core)"\
+  "\n"\
+  "(defn stream-file"\
+  "\"Takes an s3 url an creates an input stream for it's content\""\
+  "[s3Url]"\
+  "(io/input-stream (io/as-url s3Url)))"
+
+socket = send(failing_method)
+# socket = send(clojure_namespaced_method)
+
+log = []
 
 # while next_message = socket.recvmsg_nonblock
-while true
-  begin
-    next_message = socket.recvmsg_nonblock
-    @messages << decode(next_message.first)
-  rescue
-  end
-  # puts @messages
-end
+# while true
+#   next_message = socket.recvmsg_nonblock
+#   @messages << decode(next_message.first)
+#   puts @messages
+#   # puts @messages
+# end
 
+# XXX working code but not good for the project
 # puts "first"
 # puts decode(socket.recvmsg.first)
 # puts "second"
-# second_message = socket.recv # msg.first
+# second_message = socket.recvmsg # msg.first
 # puts second_message
-# puts decode(socket.recvmsg.first)
+# puts decode(second_message.first)
 
-# puts socket.methods - Object.methods
+puts socket.class
+
+catch (:complete) do
+  while true
+    message = socket.recvmsg.first
+    # XXX probs [] for nil is in decoding...
+    raw(message).each do |mess|
+      p mess.to_s
+      throw :complete if mess.to_s.include?('done')
+    end
+    # decode_all(message).each do |dict|
+    #   log << dict
+    #   throw :complete if dict['status'] == 'done'
+    # end
+  end
+end
+
+puts log
